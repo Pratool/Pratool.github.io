284c41db92a8085cd0d794781f8b00937520bde4
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2016-01-15 23:58:47 -0500

    Added more/less changer. Not using effective html styling and text yet

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,10 @@
 $('span.more').click(function() {
     //alert($(this).attr("id"));
-    $("."+$(this).attr("id")).css('display', 'block');
+    if ($("."+$(this).attr("id")).css('display') == 'none') {
+        $("."+$(this).attr("id")).css('display', 'block');
+        $(this).text('Less');
+    } else {
+        $("."+$(this).attr("id")).css('display', 'none');
+        $(this).text('More');
+    }
 });
-
-//$('.cpu-info').css('display','block')
